715039b31b64e95c80a9fe2a39df530a7839137d
Mikhail Kudashev <komandarmy@rambler.ru>
2021-02-13 20:37:08 +0300

    Add solution 0-module-1-task

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,10 +1,9 @@
-let number1;
-let number2;
-function sum(number1, namber2) {
-  summa = number1; 
-  summa = summa+namber2;
+//0-module_1-task_function of summing two numbers
+function sum(number1, number2) {
+  let summa = number1+number2;
+  return summa;  
 }
-sum(1,1);
-console.log ("sum(1,1)=",summa);
-sum(2,5);
-console.log ("sum(2,5)=",summa);
+let summa1 = sum(1,1);
+console.log ("sum(1,1)=",summa1);
+let summa2 = sum(2,5);
+console.log ("sum(2,5)=",summa2);
