6e2c195201701c6c00858515cbabb983fca224f6
Mikhail Kudashev <komandarmy@rambler.ru>
2021-02-16 20:54:22 +0300

    add solution 1-modul 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,8 +1,17 @@
-/**
- * Factorial
- * @param {number} n
- * @returns {number}
- */
+//1-module_1-task_calculating factorial
 function factorial(n) {
-  // ваш код...
+  if (n ==0 || n == 1) return 1
+    let result=1;
+    for (let num = 1; num <= n; num++) {
+         result= result * num;
+         }
+  return result;  
 }
+let factorial0 = factorial(0);
+console.log("0!=",factorial0);
+let factorial1 = factorial(1);
+console.log("1!=",factorial1);
+let factorial3 = factorial(3);
+console.log("3!=",factorial3);
+let factorial5 = factorial(5);
+console.log("5!=",factorial5);
