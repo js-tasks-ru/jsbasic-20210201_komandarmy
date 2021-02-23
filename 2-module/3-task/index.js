4ebf726521c5f809c74818cf34351c80e0ca914c
Mikhail Kudashev <komandarmy@rambler.ru>
2021-02-23 16:52:36 +0300

    add solution 2-module_3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+    
+    read: function (num1, num2) {
+      numbers = [num1,num2];
+    },
+    
+    sum: function () {
+      return numbers[0] + numbers[1];
+    },
+    
+    mul: function () {
+      return numbers[0] * numbers[1];
+    }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
