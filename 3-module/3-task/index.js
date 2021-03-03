436ac14e825d7850a3559026fd367a17a9254848
Mikhail Kudashev <komandarmy@rambler.ru>
2021-03-03 13:26:51 +0300

    added solutions 3-module_2_3_4_5-tasks

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let strArray = str.split("");
+  for (let numberStrArray = 0; numberStrArray < strArray.length; numberStrArray++) { 
+    if (strArray[numberStrArray] === "-") {
+      strArray[numberStrArray+1] = strArray[numberStrArray+1].toUpperCase();
+      strArray.splice(numberStrArray, 1);
+    };
+  }
+return strArray.join("");
 }
